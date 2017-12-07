2bc9e0c09a8cb9f052da627d1e5651957a3e21b2
brunobfranco17 <32572856+brunobfranco17@users.noreply.github.com>
2017-12-07 09:53:37 -0200

    Dados de login salvos no XML

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -6,6 +6,7 @@
 package controllers;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +16,8 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import views.LoginCredentials;
 import vo.ClienteVO;
 
@@ -48,10 +51,20 @@ public class LoginController {
                         String email = cliente.getChildText("email");
                         
                         if(email.equals(clienteVO.getEmail())){
-                            String senha = cliente.getChildText("Senha");
+                            String senha = cliente.getChildText("senha");
                             if(senha.equals(clienteVO.getSenha())){
                                 String tipoCliente = cliente.getChildText("tipo");
                                 if(tipoCliente.equals(tipo)){
+                                    
+                                    clienteVO.setNome(cliente.getChildText("nome"));
+                                    clienteVO.setSobrenome(cliente.getChildText("sobrenome"));
+                                    clienteVO.setCpf(cliente.getChildText("cpf"));
+                                    clienteVO.setDtNasc(cliente.getChildText("dataNascimento"));
+                                    clienteVO.setTipo(tipo);
+                                    clienteVO.setSexo(cliente.getChildText("sexo"));
+                                    
+                                    salvaDadosUsuario(clienteVO);
+
                                     return true;
                                 }
                             }
@@ -66,5 +79,49 @@ public class LoginController {
         }
     
         return false;
+    }
+    
+        private void salvaDadosUsuario(ClienteVO clienteVO){
+        
+        Element cliente = new Element("cliente");
+        Element nome = new Element("nome");
+        Element sobrenome = new Element("sobrenome");
+        Element tipo = new Element("tipo");
+        Element email = new Element("email");
+        Element cpf = new Element("cpf");
+        Element dtNasc = new Element("dataNascimento");
+        Element sexo = new Element("Sexo");
+        Element senha = new Element("Senha");
+
+        nome.setText(clienteVO.getNome());
+        sobrenome.setText(clienteVO.getSobrenome());
+        tipo.setText(clienteVO.getTipo());
+        email.setText(clienteVO.getEmail());
+        cpf.setText(clienteVO.getCpf());
+        dtNasc.setText(clienteVO.getDtNasc());
+        sexo.setText(clienteVO.getSexo());
+        senha.setText(clienteVO.getSenha());
+        
+        cliente.addContent(nome);
+        cliente.addContent(sobrenome);
+        cliente.addContent(tipo);
+        cliente.addContent(email);
+        cliente.addContent(cpf);
+        cliente.addContent(dtNasc);
+        cliente.addContent(sexo);
+        cliente.addContent(senha);
+            
+        Document clientesXML = new Document(cliente);
+            
+        XMLOutputter xout = new XMLOutputter();
+        FileWriter arquivo;
+        
+        try {
+            arquivo = new FileWriter(new File(localPath + "/src/main/java/data/DadosLogin.xml"));
+            xout.setFormat(Format.getPrettyFormat());
+            xout.output(clientesXML, arquivo);
+        } catch (IOException ex) {
+            Logger.getLogger(CadastroController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
